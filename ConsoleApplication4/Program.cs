e9f8fee4e274dca321ed9967628dc95ac8f52c54
drewpchannel <drewpchannel@gmail.com>
2017-03-15 13:25:07 -0500

    any

---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Npgsql;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sqlServer = new sqlLogin();
+            var conn = sqlServer.getConnection();
+            conn.Open();
+
+            string simple = "whatever'); drop table data; INSERT INTO data (some_field) VALUES ('anything";
+            sqlServer.recordCoords(simple);
+
+            var cmdRead = new NpgsqlCommand();
+            cmdRead.Connection = conn;
+            cmdRead.CommandText = "SELECT * FROM data";
+            var queryText = cmdRead.ExecuteReader();
+
+            Console.WriteLine(queryText.Read());
+            Console.WriteLine(queryText.GetString(0));
+        }
+    }
+}
---
+++ ConsoleApplication4/sqlLogin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Npgsql;
+
+namespace ConsoleApplication4
+{
+    //do you need to somehow assign a new instance?  i think c# does that for me
+    //maybe id the instance with something random an check on it?
+    class sqlLogin
+    {
+        private NpgsqlConnection _npgsqlConnection = new NpgsqlConnection("Host=34.206.79.166;Port=5432;Username=somethingiffy;Password=rugby123;Database=postgresdb");
+        private NpgsqlCommand cmd = new NpgsqlCommand();
+        public NpgsqlConnection getConnection()
+        {
+            return _npgsqlConnection;
+        }
+
+        public void recordCoords(string playerId)
+        {
+            var cmd = new NpgsqlCommand();
+            cmd.Connection = this._npgsqlConnection;
+            var cmdText = $"INSERT INTO data (some_field) VALUES ('{playerId}')";
+            Console.WriteLine(cmdText);
+            cmd.CommandText = cmdText;
+            cmd.ExecuteNonQuery();
+        }
+
+    }
+}
